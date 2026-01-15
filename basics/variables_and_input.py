35d96efb3d03661131fa6f8659fa20c6bdffd82b
esrn303 <eriosn303@gmail.com>
2026-01-15 11:18:11 -0500

    Add first Python practice script

---
+++ basics/variables_and_input.py
@@ -0,0 +1,8 @@
+# Basic Python practice
+# Variables and user input
+
+name = input("enter your name: ")
+age = input("enter your age: ")
+
+print("hello,", name)
+print("you are", age, "years old.")
